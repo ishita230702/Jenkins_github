b5757f1f2919aae56198d58678af831a2fc36b6b
ishita230702 <167290181+ishita230702@users.noreply.github.com>
2024-05-01 18:25:00 +1000

    Add files via upload

---
+++ jenkins_github.groovy
@@ -0,0 +1,44 @@
+pipeline{
+    agent any
+    environment{
+        DIRECTORY_PATH="path"
+        TESTING_ENVIRONMENT="test"
+        PRODUCTION_ENVIRONMENT="ISHITA"
+    }
+    stages{
+        stage('Build'){
+            steps{
+                echo "fetch the source code from ${DIRECTORY_PATH}"
+                echo "compile the code and generate any necessary artifacts"
+            }
+        }
+        stage('Test'){
+            steps{
+                echo "unit tests"
+                echo "integration tests"
+            }
+        }
+        stage('Code Quality Check'){
+            steps{
+                 echo "check the quality of the code"
+            }
+        }
+        stage('Deploy'){
+            steps{
+                echo "deploy the application to ${TESTING_ENVIRONMENT}"
+            }
+        }
+        stage('Approval'){
+            steps{
+                echo "Approval Begins"
+                sleep(time:10, unit: 'SECONDS')
+                echo "Approval Ended"
+            }
+        }
+        stage('Deploy to Production'){
+            steps{
+                echo "Deployment to ${PRODUCTION_ENVIRONMENT} Started and completed!"
+            }
+        }        
+    }    
+}
