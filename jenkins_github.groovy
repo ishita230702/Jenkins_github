fa6397415c4089d8d0b3b19928a342016bb33a1b
ishita230702 <167290181+ishita230702@users.noreply.github.com>
2024-05-03 00:05:59 +1000

    Update jenkins_github.groovy

--- jenkins_github.groovy
+++ jenkins_github.groovy
@@ -1,7 +1,7 @@
 pipeline{
     agent any
     environment{
-        DIRECTORY_PATH="path"
+        DIRECTORY_PATH="https://github.com/ishita230702/Jenkins_github.git"
         TESTING_ENVIRONMENT="test"
         PRODUCTION_ENVIRONMENT="ISHITA"
     }
@@ -11,40 +11,57 @@ pipeline{
                 echo "fetch the source code from ${DIRECTORY_PATH}"
                 echo "Build the code using Maven"
             }
+        }
+        stage('Unit and Integration Tests') {
+            steps {
+                echo "Run unit tests using JUnit"
+                echo "Run integration tests using Selenium"
+            }
             post{
                 success{
                     mail to:"ishu.g230702@gmail.com",
-                        subject:"Build Status email",
-                        body:"Build was successful!"
+                        subject:"Unit and Integration Test Status email",
+                        body:"Unit and Integration tests stage was successful!"
+                }
+                 failure{
+                     mail to:"ishu.g230702@gmail.com",
+                        subject:"Unit and Integration Test Status email",
+                        body:"Unit and Integration tests stage FAILURE!"
                 }
             }
         }
-        stage('Test'){
+    }
+         stage("Security scan"){
             steps{
-                echo "unit tests"
-                echo "integration tests"
+                echo "Analysing code..."
+                echo "Security scan tool: Checkmarx"
+        }
+             post{
+                success{
+                    mail to:"ishu.g230702@gmail.com",
+                        subject:"Security scan Status email",
+                        body:"Security Scan stage was successful!"
+                }
+                 failure{
+                   mail to:"ishu.g230702@gmail.com",
+                       subject:"Security scan Status email",
+                        body:"Security Scan stage FAILURE!"
+            }
+         }
+             stage('Deploy to Staging') {
+            steps {
+                echo "deploy the application t"
+                echo"Deploy tool: AWS EC2"
             }
         }
-        stage('Code Quality Check'){
-            steps{
-                 echo "check the quality of the code"
-            }
-        }
-        stage('Deploy'){
-            steps{
-                echo "deploy the application to ${TESTING_ENVIRONMENT}"
-            }
-        }
-        stage('Approval'){
-            steps{
-                echo "Approval Begins"
-                sleep(time:10, unit: 'SECONDS')
-                echo "Approval Ended"
+        stage('Integration Tests on Staging') {
+            steps {
+                echo "Run integration tests on the staging environment"
             }
         }
         stage('Deploy to Production'){
             steps{
-                echo "Deployment to ${PRODUCTION_ENVIRONMENT} Started and completed!"
+                echo "Deployment to AWS EC2. Started and completed!"
             }
         }        
     }    
